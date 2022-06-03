245c57f36716f9017cde7f91139ec69f33d42d5d
Agustin Kassis <webmaster@masize.com>
2022-06-03 16:01:39 -0300

    deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,19 +11,23 @@ async function main() {
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
-  // await hre.run('compile');
 
   // We get the contract to deploy
-  const Gateway = await ethers.getContractFactory("Gateway");
+  const GatewayFactory = await ethers.getContractFactory("GatewayFactory");
 
   const currencyAddress = "0x448aa1665fe1fae6d1a00a9209ea62d7dcd81a4b";
   const owner = "0x448aa1665fe1fae6d1a00a9209ea62d7dcd81a4b";
 
-  const gateway = await Gateway.deploy(currencyAddress, owner);
+  const gatewayFactory = await GatewayFactory.deploy();
+  await gatewayFactory.deployed();
 
-  await gateway.deployed();
+  const gateway = await gatewayFactory.createGateway(currencyAddress, owner);
 
-  console.log("Gateway deployed to:", gateway.address);
+  console.info("lenght: ", await gatewayFactory.gateswaysLength());
+
+  console.info(gateway);
+
+  // console.log("Gateway deployed to:", gateway.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
