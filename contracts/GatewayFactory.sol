df55c52f096da7e357203f32bb469ddc2bd02aad
Agustin Kassis <webmaster@masize.com>
2022-06-11 15:09:48 -0300

    fixed create2 code

--- contracts/GatewayFactory.sol
+++ contracts/GatewayFactory.sol
@@ -31,27 +31,13 @@ contract GatewayFactory {
         external
         returns (address gatewayAddress)
     {
-        // Deploy contract
-        bytes memory bytecode = type(Gateway).creationCode;
 
         bytes32 salt = keccak256(
             abi.encodePacked(block.timestamp, currencyAddress, owner_)
         );
 
-        assembly {
-            gatewayAddress := create2(
-                0,
-                add(bytecode, 32),
-                mload(bytecode),
-                salt
-            )
-
-            if iszero(extcodesize(gatewayAddress)) {
-                // revert(0, 0)
-            }
-        }
-
-        console.log("Address", gatewayAddress);
+        Gateway gateway = (new Gateway){salt: salt}(currencyAddress, owner_);
+        gatewayAddress = address(gateway);
 
         // Add gateway to gateway array
         gateways.push(
@@ -70,8 +56,6 @@ contract GatewayFactory {
             currencyAddress,
             block.timestamp
         );
-        gatewayAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        return gatewayAddress;
     }
 
     function gateswaysLength() external view returns (uint256) {
